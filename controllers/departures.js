efb6c53aedbec1bdc2c24089c108a5e876f274aa
Olli Eloranta <olli@ollieloranta.fi>
2020-11-29 08:56:32 +0200

    Query filters & setInterval added

--- controllers/departures.js
+++ controllers/departures.js
@@ -1,6 +1,9 @@
+const Bus = require('../models/bus');
+
 const axios = require('axios');
 
 exports.getDepartures = (req, res, next) => {
+   console.log('getDepartures ran')
    axios.get('https://v5.bvg.transport.rest/stops/900000023301/departures')
       .then((response) => {
          console.log(response.data)
@@ -9,6 +12,14 @@ exports.getDepartures = (req, res, next) => {
          console.log(onlyBuses);
          const busesByLine = onlyBuses.map(busDeparture => {return {line: busDeparture.line.name, delay: busDeparture.delay !== null && busDeparture.delay}})
          console.log(busesByLine);
+         let bus;
+         for (let i=0; i < busesByLine.length; i++) {
+            const busInfo = new Bus({
+               line: busesByLine[i].line,
+               delay: busesByLine[i].delay
+            })
+            busInfo.save();
+         }
       })
       .catch((error) => {
          // handle error
@@ -18,6 +29,8 @@ exports.getDepartures = (req, res, next) => {
          // always executed
    });
 }
+
+setInterval(this.getDepartures, 300000);
 
 const filterBuses = (departure) => {
    return departure.filter(d => d.line.mode === 'bus')
