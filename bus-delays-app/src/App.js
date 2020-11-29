cac42609fd6faafb7354a96818e1baebe3079f5a
Olli Eloranta <olli@ollieloranta.fi>
2020-11-29 19:05:43 +0200

    Filtering logic added to frontend

--- bus-delays-app/src/App.js
+++ bus-delays-app/src/App.js
@@ -3,33 +3,50 @@ import axios from 'axios';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Table from 'react-bootstrap/Table';
 import { useTable } from 'react-table';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   const [data, setData] = useState([]);
+  const [originalData, setOriginalData] = useState([]);
   const [lineNumbers, setLineNumbers] = useState([]);
+  const [lineNumbersLoaded, setLineNumbersLoaded] = useState(false);
+  const [filteredByLineNumbers, setFilteredByLineNumbers] = useState(false);
+  const [directions, setDirections] = useState([]);
 
   useEffect(() => {
     fetchData();
-    setInterval(() => {
-      fetchData();
-    }, 30000)
   }, [])
 
   useEffect(() => {
-    const lineNumbers = data.map(busDeparture => busDeparture.line);
+    setLineNumbersLoaded(false);
+    const lineNumbers = originalData.map(busDeparture => busDeparture.line);
     const lineNumbersWithoutDuplicates = lineNumbers.filter((lineNumber, index) => lineNumbers.indexOf(lineNumber) === index);
     setLineNumbers(lineNumbersWithoutDuplicates);
-  }, [data]);
+    setLineNumbersLoaded(true);
+  }, [originalData]);
   
   const fetchData = () => {
-    axios.get('http://localhost:8080/alldepartures')
+      axios.get('http://localhost:8080/alldepartures')
       .then(response => {
         setData(response.data.allDepartures)
+        setOriginalData(response.data.allDepartures)
       })
       .catch(error => {
         console.log(error);
       })
+  }
+
+  const filterByLineNumber = (e) => {
+    setData(originalData.filter(d => d.line === e.target.value));
+    const dirs = originalData.filter(d => d.line === e.target.value).map(d => d.direction);
+    const directionsWithoutDuplicates = dirs.filter((direction, index) => dirs.indexOf(direction) === index);
+    setDirections(directionsWithoutDuplicates);
+  }
+
+  const filterByLineNumberAndDirection = (e) => {
+    setData(originalData.filter(d => d.direction === e.target.value));
   }
 
   const columns = useMemo(() => [
@@ -68,12 +85,38 @@ const App = () => {
   return (
     <>
       <Container style={{padding: 30}}>
+        {console.log(directions)}
         <Row>
           <Col>
-            {console.log(lineNumbers)}
             <h1>Bussien myöhästymiset pysäkillä U Uhlandstraße (Berliini)</h1>
 
-   <table {...getTableProps()}>
+            <h2>Suodata tuloksia</h2>
+
+            {lineNumbersLoaded ?
+            <div>
+              <select id='linenumber' onChange={e => filterByLineNumber(e)}>
+              <option>Linjanumero:</option>
+              {lineNumbers.map((lineNumber, index) => (
+                <option key={index} value={lineNumber}>{lineNumber}</option>
+              ))}
+            </select>
+            </div>
+            :
+            <div>Ladataan linjanumeroita...</div>}
+
+            {directions ?
+            <div>
+              <select id='direction' onChange={e => filterByLineNumberAndDirection(e)}>
+              <option>Määränpää:</option>
+              {directions.map((direction, index) => (
+                <option key={index} value={direction}>{direction}</option>
+              ))}
+            </select>
+            </div>
+            :
+            <div>Ladataan määränpäitä...</div>}
+
+   <Table striped bordered hover {...getTableProps()}>
 
      <thead>
 
@@ -133,7 +176,7 @@ const App = () => {
 
                return (
 
-                 <td {...cell.getCellProps()} style={{textAlign: 'center'}}>
+                 <td {...cell.getCellProps()} >
 
                    {// Render the cell contents
 
@@ -153,7 +196,7 @@ const App = () => {
 
      </tbody>
 
-   </table>
+   </Table>
           </Col>
         </Row>
       </Container>
